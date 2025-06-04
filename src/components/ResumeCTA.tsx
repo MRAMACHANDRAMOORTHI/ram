28161d844bbd6207ae9eee9961da915d3dd661d0
MRAMACHANDRAMOORTHI <mr.ramachandramoorthi@gmail.com>
2025-06-04 12:47:34 +0530

    Resume Update

--- src/components/ResumeCTA.tsx
+++ src/components/ResumeCTA.tsx
@@ -9,7 +9,7 @@ const ResumeCTA: React.FC = () => {
     
     try {
       // Direct link to your Google Drive file for download
-      const resumeUrl = "https://drive.google.com/file/d/1Gm50Djawd80gOccY9iXFUA_-jf_o9twT/view?usp=sharing";
+      const resumeUrl = "https://drive.google.com/file/d/16c2uqGcmIqdte1nz8DvBWaz5XE6c_wF2/view?usp=sharing";
       
       // Create an anchor element
       const link = document.createElement('a');
